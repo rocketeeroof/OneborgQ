b7c53f6ece053778f03267ffb5070e0ada1ae959
Scumclass <93349787+rocketeeroof@users.noreply.github.com>
2026-09-14 22:27:38 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -47,7 +47,7 @@ class MyBot(Bot):
         ball_location = Vec3(packet.balls[0].physics.location)
         ball_velocity = Vec3(packet.balls[0].physics.velocity)
         goal_location = Vec3(0, 5120 * (1 - 2 * self.team), 0)
-
+        # Skibidi
         # By default we will chase the ball, but target_location can be changed later
         target_location = ball_location
 
